fcb33db25e9405ca019c502242e6c7090eeb01d9
Randika Lakmal <randika.l.abeyrathna@gmail.com>
2023-10-07 09:47:43 +0530

    Init Commit

---
+++ src/main/java/com/randikalakmal/Springbootelk/SpringbootelkApplication.java
@@ -0,0 +1,13 @@
+package com.randikalakmal.Springbootelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootelkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootelkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randikalakmal/Springbootelk/SpringbootelkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randikalakmal.Springbootelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootelkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
